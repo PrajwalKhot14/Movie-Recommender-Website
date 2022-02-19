03f571072bb9ddc55b535774afa7a01db6e41009
Prajwal <khotprajwal04@gmail.com>
2022-02-19 02:10:25 -0600

    Final commit

--- Python_files/app.py
+++ Python_files/app.py
@@ -9,7 +9,6 @@ def fetch_poster(movie_id):
         'https://api.themoviedb.org/3/movie/{}?api_key=27d7075c356c6056df4ffe06ce51a213&language=en-US'.format(
             movie_id))
     data = response.json()
-    print(data)
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 
