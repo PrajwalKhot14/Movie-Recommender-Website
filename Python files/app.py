5201dcb1d9ff4e5441d606af618bb4b13cdc452e
Prajwal <khotprajwal04@gmail.com>
2022-02-19 01:20:34 -0600

    Update app.py

--- Python files/app.py
+++ Python files/app.py
@@ -1,14 +1,24 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import requests
 
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=27d7075c356c6056df4ffe06ce51a213&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+    
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movie_list = sorted(list(enumerate(similarity[movie_index])), reverse=True, key=lambda x: x[1])[1:6]
     recommended_movies = []
+    recommended_movies_poster = []
     for i in movie_list:
-        recommended_movies.append(movies.iloc[i[0]].title)
+        movie_id = i[0]
+
+        recommended_movies.append(movies.iloc[movie_id].title)
+        recommended_movies_poster.append(fetch_poster(movie_id))
     return recommended_movies
 
 similarity  = pickle.load(open('similarity.pkl', 'rb'))
@@ -20,6 +30,6 @@ st.title('Movie Recommender System')
 selected_movie = st.selectbox('Select a movie', ( movies['title'].values))
 
 if st.button('Recommend'):
-    recommendations = recommend(selected_movie)
-    for i in recommendations:
+    names, poster = recommend(selected_movie)
+    for i in names:
         st.write(i)
