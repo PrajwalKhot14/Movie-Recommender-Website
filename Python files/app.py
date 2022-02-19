833e3608247155db6b424bde13f39e83a4cbbd34
Prajwal <khotprajwal04@gmail.com>
2022-02-19 01:30:16 -0600

    Update app.py

--- Python files/app.py
+++ Python files/app.py
@@ -6,6 +6,7 @@ import requests
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=27d7075c356c6056df4ffe06ce51a213&language=en-US'.format(movie_id))
     data = response.json()
+    print(data)
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
     
 def recommend(movie):
@@ -15,11 +16,11 @@ def recommend(movie):
     recommended_movies = []
     recommended_movies_poster = []
     for i in movie_list:
-        movie_id = i[0]
+        movie_id = movies.iloc[i[0]].movie_id
 
-        recommended_movies.append(movies.iloc[movie_id].title)
+        recommended_movies.append(movies.iloc[i[0]].title)
         recommended_movies_poster.append(fetch_poster(movie_id))
-    return recommended_movies
+    return recommended_movies, recommended_movies_poster
 
 similarity  = pickle.load(open('similarity.pkl', 'rb'))
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
@@ -31,5 +32,24 @@ selected_movie = st.selectbox('Select a movie', ( movies['title'].values))
 
 if st.button('Recommend'):
     names, poster = recommend(selected_movie)
-    for i in names:
-        st.write(i)
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(names[0])
+        st.image(poster[0])
+
+    with col2:
+        st.text(names[1])
+        st.image(poster[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(poster[2])
+
+    with col4:
+        st.text(names[3])
+        st.image(poster[3])
+
+    with col5:
+        st.text(names[4])
+        st.image(poster[4])
